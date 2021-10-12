178848a8f3cfe3875cba4519f172129695e2b308
Alcatraz Lynn <32809811+Alcatraz1337@users.noreply.github.com>
2021-10-12 15:03:01 +0800

    Fixed a bug where the program uses a wrong path

--- rdodaily.py
+++ rdodaily.py
@@ -21,9 +21,6 @@ def GetAllChallenges(soup, file):
     allChallenges = soup.find_all('span', class_='challenge-input-container-label')
     allChallengeGoals = soup.find_all('span', class_='challenge-goal')
 
-    if not os.path.exists("Archive"):
-        os.mkdir("Archive")
-
     with open(file, "w+", encoding="utf-8") as f:
         for i in range(len(allChallenges)):
             if len(re.split(r'[\n]', allChallenges[i].label.get_text())) > 1:
@@ -210,8 +207,11 @@ def FormatFile(file):
 
 def main():
     date = input("Enter date (YYYY-MM-DD): ")
-    dateOfFile = "\Archive\daily"+date+".txt"
-    file = os.path.join(os.path.dirname(__file__) + dateOfFile)
+    if not os.path.exists("Archive"):
+        os.mkdir("Archive")
+    dateOfFile = "\Archive\daily-"+date+".txt"
+    file = os.path.dirname(os.path.abspath(__file__)) + dateOfFile
+    print(file)
     url = GenerateURL(date)
     soup = ReadHTML(url)
     GetAllChallenges(soup, file)
